9bd70beac9a99349071384fe5e4c4ac16a00520f
Angel Zuniga <angelzuniga@outlook.com>
2020-04-05 09:05:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,30 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creater(attendees)
-  attendees.each do |name|
-  puts "Hello, my name is #{name}"
+
+def batch_badge_creator(speakers)
+  speakers.map do |guest|
+    "Hello, my name is #{guest}."
   end
 end
+
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(speakers)
+  
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end 
