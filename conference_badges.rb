5ae709dc2b7077edada63370cf1c5e76c95b45f0
Angel Zuniga <angelzuniga@outlook.com>
2020-04-05 08:58:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
-attendees = ["Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+
 
 def batch_badge_creater(attendees)
   attendees.each do |name|
